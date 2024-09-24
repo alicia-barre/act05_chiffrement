42f3c940c38d43d70e4b0c4b2d660aa1a70ddd93
alicia-barre <alicia.barre2005@gmail.com>
2024-09-24 17:40:28 +0200

    Update cryptoCielTest.cpp
    chiffrage hybride

--- cryptoCielTest.cpp
+++ cryptoCielTest.cpp
@@ -17,24 +17,29 @@ int main()
 	HashGestion LM;
 	std::cout << LM.CalculateSHA256(monMessageHash) << std::endl;
 
-	AesGestion monAES;
+	/*AesGestion monAES;
 	monAES.GenerateAESKey();
 	monAES.SaveAESKeyToFile("ma_clef_aes.key");
 	monAES.LoadAESKeyFromFile("ma_clef_aes.key");
 	monAES.EncryptFileAES256("ma_clef_aes.key", "ma_clef_aes_crypt.key");
-	monAES.DecryptFileAES256("ma_clef_aes_crypt.key", "ma_clef_aes_decrypt.key");
+	monAES.DecryptFileAES256("ma_clef_aes_crypt.key", "ma_clef_aes_decrypt.key");*/
+
 
 	RsaGestion monRSA;
 	//monRSA.generationClef("clef_public_alicia.key", "clef_prive_alicia.key", 2048);
 	monRSA.chargementClefs("clef_public_alicia.key", "clef_prive_alicia.key");
+	//monRSA.chiffreDansFichier("salut", "fichier_chiffre_rsa_alicia.crypt");
+	monRSA.dechiffrementFichier("LPAESKey.crypt", "aes.decrypt", true);
 
-	monRSA.chiffreDansFichier("salut", "fichier_chiffre_rsa_alicia.crypt");
-	monRSA.dechiffrementFichier("fichier_chiffre_rsa_alicia.crypt", "fichier_dechiffre_rsa_alicia.decrypt", true);
 	std::string message = "hello";
 	std::string MessageCrypt = monRSA.chiffrementRsa(message);
 	std::cout << MessageCrypt << std::endl;
 	std::string MessageDecrypt = monRSA.dechiffrementRsa(MessageCrypt);
 	std::cout << MessageDecrypt << std::endl;
+
+	AesGestion monAES;
+	monAES.LoadAESKeyFromFile("aes.decrypt");
+	monAES.EncryptFileAES256("SECRET.txt", "output.crypt");
 
 
 
